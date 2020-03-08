c148e4e52c8c6977a05869d07a533e09c1ddaa10
Amanoman <amntkykblog@gmail.com>
2020-03-08 23:08:53 +0900

    3月8日更新

--- Python/SEDORI/mercari.py
+++ Python/SEDORI/mercari.py
@@ -1,11 +1,28 @@
 #%%
-import requests
-from bs4 import BeautifulSoup
+pip install chromedriver-binary selenium
+#%%
+import sys
+import time
+from selenium import webdriver
+import pandas
 
-load_url = "https://www.mercari.com/jp/"
-html = requests.get(load_url)
-soup = BeautifulSoup(html.content, "html.parser")
+# %%
+# 引数から取得 実行例：python merscraping.py
+args = sys.argv
+query = args[1]
 
-print(soup)
+# %%
+# 別途ダウンロードしたchromedriver.exeの場所を指定
+#Macの場合chromedriver winの場合chromedriver.exe
+browser = webdriver.Chrome(executable_path="chromedriver")
+browser.implicitly_wait = 10
 
-＃メルカリはスクレイピング禁止のためrequetsではサーバで弾かれて403エラー
+url_site = "https://www.mercari.com/jp/".format(query)
+browser.get(url_site)
+
+page = 1
+item_num = 0
+urls = []
+
+
+# %%
