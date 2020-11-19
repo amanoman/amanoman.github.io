c1efd0f388ef268591f7b40b276c936d39cf26e7
Amanoman <amntkykblog@gmail.com>
2020-11-19 21:20:01 +0900

    pyfxtrading change

--- Python/Udemy/Fintech/pyfxtrading/settings.py
+++ Python/Udemy/Fintech/pyfxtrading/settings.py
@@ -5,6 +5,6 @@ import configparser
 conf = configparser.ConfigParser()
 conf.read('settings.ini')
 
-acount_id = conf['oanda']['acount_id']
+accunt_id = conf['oanda']['acount_id']
 access_token = conf['oanda'][access_token]
 
