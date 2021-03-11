de7c1147194a594239b2129e2859e28248ea600e
aman <amntkykblog@gmail.com>
2021-03-11 15:51:27 +0900

    saitann add

---
+++ saitan/ditplay_message.py
@@ -0,0 +1,10 @@
+def display_message():
+    print("この章では関数の宣言の仕方を学んでいます")
+
+def favorite_book(title):
+    print(f"私の好きな本は{title}です")
+
+favorite_book("不思議の国のアリス")
+
+
+display_message()
---
+++ saitan/greeter.py
@@ -0,0 +1,5 @@
+def greet_user(username):
+    """シンプルなあいさつメッセージを出力する"""#ドックストリングでドキュメント生成
+    print(f"こんにちは{username.title()}！")
+
+greet_user("あまん")
