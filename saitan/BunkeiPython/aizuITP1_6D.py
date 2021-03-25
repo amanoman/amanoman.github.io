790aafd2be0b7f430ecd7870ad6218bc3f7a81ab
Amanoman <amntkykblog@gmail.com>
2021-03-26 00:13:08 +0900

    aizu add

--- saitan/BunkeiPython/aizuITP1_6D.py
+++ saitan/BunkeiPython/aizuITP1_6D.py
@@ -1,16 +1,15 @@
 n,m = map(int,input().split())
-A_lists=[]
-b_lists=[]
+A_lists=[[]for i in range(n)]
 for i in range(n):
-    A_lists[i-1] = list(map(int,input().split())
-    #A_lists.append(A)
-#print(A_lists)
-for j in range(m):
-    b = int(input())
-    b_lists.append(b)
+    A_lists[i]=list(map(int,input().split()))
+
+b_lists=[[0]*1 for i in range(m)]
+for i in range(m):
+    b_lists[i] = int(input())
+print(A_lists)
 print(b_lists)
-c = 0
-for k in range(m):
-    C += A_lists[k] * b_lists[k]
-    print(C)
-#table [[for i in range()]]
+for row in range(n):
+    tmp = 0
+    for col in range(m):
+        tmp += A_lists[row][col]*b_lists[col]
+    print(tmp)
